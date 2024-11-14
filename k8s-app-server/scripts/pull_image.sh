563bbd667b974be9c0e320cafc122a217552cdef
IhebBelhadjNexits <iheb.belhaj@nexits.io>
2024-11-14 17:06:31 +0100

    feat(env_setup): change docker compose file

--- k8s-app-server/scripts/pull_image.sh
+++ k8s-app-server/scripts/pull_image.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "$(date) - Received request with token: $1" >>/var/log/webhook.log
+sudo chmod +x /usr/local/bin/docker-compose
 
 cd /vagrant
-docker-compose pull >>/var/log/webhook.log 2>&1
-docker-compose up -d >>/var/log/webhook.log 2>&1
-
+# docker-compose down >>/var/log/webhook.log 2>&1
+# docker-compose pull >>/var/log/webhook.log 2>&1
+# docker-compose up -d >>/var/log/webhook.log 2>&1
+#
+echo "Pulling app..."
 echo "$(date) - Webhook execution completed" >>/var/log/webhook.log
